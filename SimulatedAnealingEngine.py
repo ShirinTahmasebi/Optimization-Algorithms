1ae4be40ad08d145853e7ff832d9d32caf02d564
ShirinTahmasebi <shirin_tahmasebi94@yahoo.com>
2019-03-15 16:15:03 +0330

    Revise SA Algorithm Parameters

--- SimulatedAnealingEngine.py
+++ SimulatedAnealingEngine.py
@@ -5,32 +5,36 @@ from Knapsack import Knapsack
 
 temperature_initial = 100
 temperature_final = 1
-cooling_rate = 0.9
-number_of_iterations = 100  # Equilibrium State
+cooling_rate = 0.99
+number_of_iterations = 1000  # Equilibrium State
 
 temperature = temperature_initial
 iteration_no = 0
 knapsack = Knapsack()
-best_solution = knapsack.generate_initial_solution()
+current_solution = knapsack.generate_initial_solution()
 
 while temperature > temperature_final:
     while iteration_no < number_of_iterations:
         iteration_no += 1
-        candidate_solution = knapsack.generate_neighbour()
-        delta = knapsack.calculate_total_price(candidate_solution) - knapsack.calculate_total_price(best_solution)
+        previous_solution = current_solution.copy()
+        current_solution = knapsack.generate_neighbour(current_solution)
+        delta = knapsack.calculate_total_price(current_solution) - knapsack.calculate_total_price(previous_solution)
         if delta > 0:
-            best_solution = candidate_solution.copy()
+            # Accept Solution (It has better price)
+            pass
         else:
             rand_prob = random()
-            if rand_prob > math.exp(float(delta) / temperature):
-                knapsack.revert_solution()
-        # print("-- Total Weight: " + str(knapsack.calculate_total_weight(best_solution)))
-        print("-- Total Price: " + str(knapsack.calculate_total_price(best_solution)))
+            if rand_prob < math.exp(float(delta) / temperature):
+                # Accept Worse Solution
+                pass
+            else:
+                # Reverse Selection (Reject)
+                current_solution = previous_solution.copy()
+        print("Current Solution Price: " + str(knapsack.calculate_total_price(current_solution)))
         break
     temperature *= cooling_rate
 
 knapsack.print_items()
-knapsack.print_solution(best_solution)
-knapsack.print_solution(best_solution)
-print("Total Weight: " + str(knapsack.calculate_total_weight(best_solution)))
-print("Total Price: " + str(knapsack.calculate_total_price(best_solution)))
+knapsack.print_solution(current_solution)
+print("Final Solution Weight: " + str(knapsack.calculate_total_weight(current_solution)))
+print("Final Solution Price: " + str(knapsack.calculate_total_price(current_solution)))
