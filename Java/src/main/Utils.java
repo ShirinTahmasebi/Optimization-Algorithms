283f0a6cc2324ebfc0730bb4d46bf048f5087a75
ShirinTahmasebi <shirin_tahmasebi94@yahoo.com>
2019-06-09 18:32:12 +0430

    Refactoring

--- Java/src/main/Utils.java
+++ Java/src/main/Utils.java
@@ -2,6 +2,7 @@ package main;
 
 import java.util.LinkedList;
 import java.util.List;
+
 import main.dijkstra.DijkstraAlgorithm;
 import main.model.Graph;
 import main.model.Vertex;
@@ -21,15 +22,15 @@ public class Utils {
         );
         return (maxSinkCoverage * sensorNumbers
                 - totalCoverSinksScore(
-                        graph, maxSinkCoverage, sinkYSpinVariables,
-                        candidateSinks, tempSinkXSpinVariables,
-                        candidateControllers, tempControllerXSpinVariables
-                )) + (maxControllerCoverage * sensorNumbers
+                graph, maxSinkCoverage, sinkYSpinVariables,
+                candidateSinks, tempSinkXSpinVariables,
+                candidateControllers, tempControllerXSpinVariables
+        )) + (maxControllerCoverage * sensorNumbers
                 - totalCoverControllersScore(
-                        graph, maxControllerCoverage, controllerYSpinVariables,
-                        candidateSinks, tempSinkXSpinVariables,
-                        candidateControllers, tempControllerXSpinVariables
-                ));
+                graph, maxControllerCoverage, controllerYSpinVariables,
+                candidateSinks, tempSinkXSpinVariables,
+                candidateControllers, tempControllerXSpinVariables
+        ));
     }
 
     public static float getLoadBalancingEnergy(
@@ -48,11 +49,11 @@ public class Utils {
 
         float controllersLoadBalancingEnergy
                 = getControllersLoadBalancingEnergy(
-                        graph, controllerYSpinVariables,
-                        candidateSinks, tempSinkXSpinVariables,
-                        candidateControllers, tempControllerXSpinVariables,
-                        maxControllerLoad, maxControllerCoverage
-                );
+                graph, controllerYSpinVariables,
+                candidateSinks, tempSinkXSpinVariables,
+                candidateControllers, tempControllerXSpinVariables,
+                maxControllerLoad, maxControllerCoverage
+        );
         return sinksLoadBalancingEnergy + controllersLoadBalancingEnergy;
     }
 
@@ -64,8 +65,7 @@ public class Utils {
         for (int i = 0; i < candidateSinks.size(); i++) {
             for (int j = 0; j < candidateControllers.size(); j++) {
                 if (tempSinkXSpinVariables[i] && tempControllerXSpinVariables[j]) {
-                    float cost = (costSink + costController) * costReductionFactor;
-                    return cost;
+                    return (costSink + costController) * costReductionFactor;
                 } else if (tempSinkXSpinVariables[i]) {
                     return costSink;
                 } else if (tempControllerXSpinVariables[j]) {
@@ -98,7 +98,6 @@ public class Utils {
             boolean[] tempSinkXSpinVariables, List<Vertex> candidateSinks,
             boolean[] tempControllerXSpinVariables, List<Vertex> candidateControllers
     ) {
-        boolean isSinkOrController = false;
         for (int i = 0; i < tempSinkXSpinVariables.length; i++) {
             boolean tempSinkXSpinVariable = tempSinkXSpinVariables[i];
             if (tempSinkXSpinVariable) {
@@ -118,7 +117,7 @@ public class Utils {
                 }
             }
         }
-        return isSinkOrController;
+        return false;
     }
 
     public static float getSinksLoadBalancingEnergy(
@@ -130,10 +129,10 @@ public class Utils {
         for (int j = 0; j < candidateSinks.size(); j++) {
             float totalLoadToJthSink
                     = calculateLoadToJthSink(
-                            j, graph, sinkYSpinVariables,
-                            tempSinkXSpinVariables, candidateSinks,
-                            tempControllerXSpinVariables, candidateControllers
-                    );
+                    j, graph, sinkYSpinVariables,
+                    tempSinkXSpinVariables, candidateSinks,
+                    tempControllerXSpinVariables, candidateControllers
+            );
             float bestSinkLoad = maxSinkLoad / (maxSinkCoverage - 1);
             totalSinkLoadEnergy += Math.max(0, totalLoadToJthSink - bestSinkLoad);
         }
@@ -149,9 +148,9 @@ public class Utils {
         for (int j = 0; j < candidateControllers.size(); j++) {
             float totalLoadToJthController
                     = calculateLoadToJthController(j, graph, controllerYSpinVariables,
-                            tempSinkXSpinVariables, candidateSinks,
-                            tempControllerXSpinVariables, candidateControllers
-                    );
+                    tempSinkXSpinVariables, candidateSinks,
+                    tempControllerXSpinVariables, candidateControllers
+            );
             float bestControllerLoad = maxControllerLoad / (maxControllerCoverage - 1);
             totalControllerLoadEnergy += Math.max(0, totalLoadToJthController - bestControllerLoad);
         }
@@ -203,8 +202,9 @@ public class Utils {
         return totalLoadToJthController;
     }
 
+    @SuppressWarnings("unused")
     public static int coveredNodesCountByController(int controllerIndex, Graph graph,
-            boolean[][] controllerYSpinVariables, boolean[] tempControllerXSpinVariables) {
+                                                    boolean[][] controllerYSpinVariables, boolean[] tempControllerXSpinVariables) {
         int coveredControllers = 0;
         for (int j = 0; j < graph.getVertexes().size(); j++) {
             coveredControllers += (controllerYSpinVariables[j][controllerIndex]
@@ -236,8 +236,9 @@ public class Utils {
         return coveredControllers;
     }
 
+    @SuppressWarnings("unused")
     public static int coveredNodesCountBySink(int sinkIndex, Graph graph,
-            boolean[][] sinkYSpinVariables, boolean[] tempSinkXSpinVariables) {
+                                              boolean[][] sinkYSpinVariables, boolean[] tempSinkXSpinVariables) {
         int coveredSinks = 0;
         for (int j = 0; j < graph.getVertexes().size(); j++) {
             coveredSinks += (sinkYSpinVariables[j][sinkIndex] && tempSinkXSpinVariables[sinkIndex]) ? 1 : 0;
@@ -292,32 +293,24 @@ public class Utils {
             List<Vertex> candidateSinks, boolean[][] sinkYSpinVariables,
             List<Vertex> candidateControllers, boolean[][] controllerYSpinVariables) {
         // Print graph
-        graph.getVertexes().stream().forEach((vertex) -> {
-            System.out.println("Vertex: " + vertex.toString());
-        });
+        graph.getVertexes().stream().forEach((vertex) -> System.out.println("Vertex: " + vertex.toString()));
 
         System.out.println();
 
-        graph.getEdges().stream().forEach((edge) -> {
-            System.out.println("Edge: " + edge.toString());
-        });
+        graph.getEdges().stream().forEach((edge) -> System.out.println("Edge: " + edge.toString()));
 
         System.out.println();
 
         // Print candidate sinks
         System.out.print("Candidate sink vertexes are: ");
-        candidateSinks.stream().forEach((candidateSinkVertex) -> {
-            System.out.print(candidateSinkVertex.toString() + ", ");
-        });
+        candidateSinks.stream().forEach((candidateSinkVertex) -> System.out.print(candidateSinkVertex.toString() + ", "));
 
         System.out.println();
         System.out.println();
 
         // Print candidate controllers
         System.out.print("Candidate controller vertexes are: ");
-        candidateControllers.stream().forEach((candidateControllerVertex) -> {
-            System.out.print(candidateControllerVertex.toString() + ", ");
-        });
+        candidateControllers.stream().forEach((candidateControllerVertex) -> System.out.print(candidateControllerVertex.toString() + ", "));
 
         System.out.println();
         System.out.println();
@@ -348,14 +341,14 @@ public class Utils {
         // --- Print temp lists
         System.out.println();
         System.out.println("Temp Sink X: ");
-        for (int i = 0; i < tempSinkXSpinVariables.length; i++) {
-            System.out.print(tempSinkXSpinVariables[i] + ", ");
+        for (boolean tempSinkXSpinVariable : tempSinkXSpinVariables) {
+            System.out.print(tempSinkXSpinVariable + ", ");
         }
 
         System.out.println();
         System.out.println("Temp Controller X: ");
-        for (int i = 0; i < tempControllerXSpinVariables.length; i++) {
-            System.out.print(tempControllerXSpinVariables[i] + ", ");
+        for (boolean tempControllerXSpinVariable : tempControllerXSpinVariables) {
+            System.out.print(tempControllerXSpinVariable + ", ");
         }
 
         System.out.println();
@@ -385,15 +378,15 @@ public class Utils {
         // --- Initialize Y and YPrime Spin Variables
         for (int i = 0; i < graph.getVertexes().size(); i++) {
             for (int j = 0; j < candidateSinks.size(); j++) {
-                // The following line can be replaced with vertexIndex = i - but I prefered to write this in the following way for more readability
+                // The following line can be replaced with vertexIndex = i - but I preferred to write this in the following way for more readability
                 int vertexIndex1 = graph.getVertexIndexById(graph.getVertexes().get(i).getId());
-                int vertexIndex2 = graph.getVertexIndexById(((Vertex) candidateSinks.get(j)).getId());
+                int vertexIndex2 = graph.getVertexIndexById(candidateSinks.get(j).getId());
                 sinkYSpinVariables[i][j] = Utils.isDistanceFavorable(graph, vertexIndex1, vertexIndex2, sensorSinkMaxDistance);
             }
             for (int j = 0; j < candidateControllers.size(); j++) {
-                // The following line can be replaced with vertexIndex = i - but I prefered to write this in the following way for more readability
+                // The following line can be replaced with vertexIndex = i - but I preferred to write this in the following way for more readability
                 int vertexIndex1 = graph.getVertexIndexById(graph.getVertexes().get(i).getId());
-                int vertexIndex2 = graph.getVertexIndexById(((Vertex) candidateControllers.get(j)).getId());
+                int vertexIndex2 = graph.getVertexIndexById(candidateControllers.get(j).getId());
                 controllerYSpinVariables[i][j] = Utils.isDistanceFavorable(graph, vertexIndex1, vertexIndex2, sensorControllerMaxDistance);
             }
         }
